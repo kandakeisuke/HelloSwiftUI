eda37e9cbef12f85152b17fb378c45b0fdcf6bc9
kandakeisuke <keichan0223.pc@gmail.com>
2024-03-28 17:50:24 +0900

    Initial Commit

---
+++ HelloSwiftUI/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by human on 2024/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var str = "Hello, SwiftUI"
+    
+    var body: some View {
+        
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text(str)
+//            Button("ボタン") {
+//                print("ボタンが押されたよ")
+//                str = "ハローSwiftUI!"
+//            }
+//            .padding()
+//        }
+//
+//        .padding()
+        ZStack{
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 200, height: 200)
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 150, height: 150)
+            Rectangle()
+                .foregroundColor(.yellow)
+                .frame(width: 100, height: 100)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by human on 2024/03/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
