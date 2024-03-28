248e2dc4314f24ae7bca707b0042288bb4e8f108
kandakeisuke <keichan0223.pc@gmail.com>
2024-03-28 19:12:17 +0900

    オブジェクト作った

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -25,17 +25,60 @@ struct ContentView: View {
 //        }
 //
 //        .padding()
-        HStack{
+//        HStack{
+//            Rectangle()
+//                .foregroundColor(.blue)
+//                .frame(width: 200, height: 200)
+//            Rectangle()
+//                .foregroundColor(.red)
+//                .frame(width: 150, height: 150)
+//            Rectangle()
+//                .foregroundColor(.yellow)
+//                .frame(width: 100, height: 100)
+//        }
+        VStack {
             Rectangle()
-                .foregroundColor(.blue)
-                .frame(width: 200, height: 200)
+                .foregroundColor(.gray)
+                .frame(width: 350, height: 100)
             Rectangle()
-                .foregroundColor(.red)
-                .frame(width: 150, height: 150)
-            Rectangle()
-                .foregroundColor(.yellow)
-                .frame(width: 100, height: 100)
+                .foregroundColor(.purple)
+                .frame(width: 350, height: 100)
+            HStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 150, height: 150)
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 100, height: 100)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 50, height: 50)
+            }
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: 350, height: 200)
+                HStack {
+                    VStack {
+                        Rectangle()
+                            .foregroundColor(.pink)
+                            .frame(width: 150, height: 70)
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 150, height: 70)
+                    }
+                    VStack {
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 70)
+                        Rectangle()
+                            .foregroundColor(.yellow)
+                            .frame(width: 150, height: 70)
+                    }
+                }
+            }
         }
+
     }
 }
 
