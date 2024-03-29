31266b19111dd2ba551c85747700bd3f32b0fcdf
kandakeisuke <keichan0223.pc@gmail.com>
2024-03-29 12:27:27 +0900

    Part4完全修了

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -19,7 +19,9 @@ struct ContentView: View {
                 print("ボタンが押されたよ")
             }
             
-            Image("kids")
+            Image("Kids")
+                .resizable(capInsets: EdgeInsets(top: -2.0, leading: 0.0, bottom: -2.0, trailing: 0.0))
+                
             
             List {
                 Text("りんご")
